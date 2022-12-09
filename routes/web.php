55e6c2356303b51aabb1823aa06bb0a2234c9c56
MrSuPilun <phamngocan01627@gmail.com>
2022-12-09 21:55:02 +0700

    LL002 - Setup Muti Auth

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.admin_dashboard');
-});
+Route::prefix('/admin')->namespace('\App\Http\Controllers\Admin')->group(function () {
+    Route::match(['GET', 'POST'], '/', 'AdminController@login')->name('admin.login');
 
-Route::get('/admin', function () {
-    return view('admin.admin_login');
+    Route::middleware(['auth:admin'])->group(function () {
+        Route::get('dashboard', 'AdminController@dashboard')->name('admin.dashboard');
+        Route::get('logout', 'AdminController@logout')->name('admin.logout');
+    });
 });
