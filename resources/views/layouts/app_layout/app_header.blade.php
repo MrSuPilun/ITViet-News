b26128f0cd1846a40ac932a75e5f67eb33556652
MrSuPilun <phamngocan01627@gmail.com>
2022-12-11 15:52:59 +0700

    LL003 - Edit home page

--- resources/views/layouts/app_layout/app_header.blade.php
+++ resources/views/layouts/app_layout/app_header.blade.php
@@ -4,19 +4,6 @@
         <div class="topbar">
             <div class="content-topbar container h-100">
                 <div class="left-topbar">
-                    <span class="left-topbar-item flex-wr-s-c">
-                        <span>
-                            New York, NY
-                        </span>
-
-                        <img class="m-b-1 m-rl-8" src="{{ asset('assets/app/images/icons/icon-night.png') }}"
-                            alt="IMG">
-
-                        <span>
-                            HI 58° LO 56°
-                        </span>
-                    </span>
-
                     <a href="#" class="left-topbar-item">
                         About
                     </a>
