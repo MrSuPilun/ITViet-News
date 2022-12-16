892842d83dc413a56f8ae2571bd8372fb2bda6e8
MrSuPilun <phamngocan01627@gmail.com>
2022-12-16 17:07:51 +0700

    LL011 - Return Home When Register User

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -16,7 +16,7 @@
 @endsection
 
 @section('content')
-    <section class="bg0 p-t-70">
+    <section class="bg0 p-b-140 p-t-10">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-8">
@@ -140,70 +140,8 @@
                     </div>
                 </div>
 
-                <div class="col-md-10 col-lg-4">
-                    <div class="p-l-10 p-rl-0-sr991 p-b-20">
-                        <!-- Stay Connected -->
-                        <div class="p-b-35">
-                            <div class="how2 how2-cl4 flex-s-c">
-                                <h3 class="f1-m-2 cl3 tab01-title">
-                                    Stay Connected
-                                </h3>
-                            </div>
-
-                            <ul class="p-t-35">
-                                <li class="flex-wr-sb-c p-b-20">
-                                    <a href="#"
-                                        class="size-a-8 flex-c-c borad-3 size-a-8 bg-facebook fs-16 cl0 hov-cl0">
-                                        <span class="fab fa-facebook-f"></span>
-                                    </a>
-
-                                    <div class="size-w-3 flex-wr-sb-c">
-                                        <span class="f1-s-8 cl3 p-r-20">
-                                            6879 Fans
-                                        </span>
-
-                                        <a href="#" class="f1-s-9 text-uppercase cl3 hov-cl10 trans-03">
-                                            Like
-                                        </a>
-                                    </div>
-                                </li>
-
-                                <li class="flex-wr-sb-c p-b-20">
-                                    <a href="#"
-                                        class="size-a-8 flex-c-c borad-3 size-a-8 bg-twitter fs-16 cl0 hov-cl0">
-                                        <span class="fab fa-twitter"></span>
-                                    </a>
-
-                                    <div class="size-w-3 flex-wr-sb-c">
-                                        <span class="f1-s-8 cl3 p-r-20">
-                                            568 Followers
-                                        </span>
-
-                                        <a href="#" class="f1-s-9 text-uppercase cl3 hov-cl10 trans-03">
-                                            Follow
-                                        </a>
-                                    </div>
-                                </li>
-
-                                <li class="flex-wr-sb-c p-b-20">
-                                    <a href="#"
-                                        class="size-a-8 flex-c-c borad-3 size-a-8 bg-youtube fs-16 cl0 hov-cl0">
-                                        <span class="fab fa-youtube"></span>
-                                    </a>
-
-                                    <div class="size-w-3 flex-wr-sb-c">
-                                        <span class="f1-s-8 cl3 p-r-20">
-                                            5039 Subscribers
-                                        </span>
-
-                                        <a href="#" class="f1-s-9 text-uppercase cl3 hov-cl10 trans-03">
-                                            Subscribe
-                                        </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-
+                <div class="col-md-10 col-lg-4 p-b-30">
+                    <div class="p-l-10 p-rl-0-sr991 p-t-33">
                         <!-- Most Popular -->
                         <div class="p-b-30">
                             <div class="how2 how2-cl4 flex-s-c">
@@ -268,8 +206,7 @@
                         <!--  -->
                         <div class="flex-c-s p-t-8 p-b-65">
                             <a href="#">
-                                <img class="max-w-full" src="{{ asset('assets/app/images/banner-02.jpg') }}"
-                                    alt="IMG">
+                                <img class="max-w-full" src="{{ asset('assets/app/images/banner-02.jpg') }}" alt="IMG">
                             </a>
                         </div>
 
@@ -313,26 +250,6 @@
                                     </span>
                                 </div>
                             </div>
-                        </div>
-
-                        <!-- Subscribe -->
-                        <div class="bg10 p-rl-35 p-t-28 p-b-35 m-b-55">
-                            <h5 class="f1-m-5 cl0 p-b-10">
-                                Subscribe
-                            </h5>
-
-                            <p class="f1-s-1 cl0 p-b-25">
-                                Get all latest content delivered to your email a few times a month.
-                            </p>
-
-                            <form class="size-a-9 pos-relative">
-                                <input class="s-full f1-m-6 cl6 plh9 p-l-20 p-r-55" type="text" name="email"
-                                    placeholder="Email">
-
-                                <button class="size-a-10 flex-c-c ab-t-r fs-16 cl9 hov-cl10 trans-03">
-                                    <i class="fa fa-arrow-right"></i>
-                                </button>
-                            </form>
                         </div>
 
                         <!-- Tag -->
