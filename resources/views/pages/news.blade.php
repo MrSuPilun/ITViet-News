0356534d56b1445d3672d344d2eda935320e00d7
MrSuPilun <phamngocan01627@gmail.com>
2022-12-19 14:53:01 +0700

    LL012 - Fix Comment

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -18,8 +18,8 @@
         }
 
         /* .comment-text {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                font-size: 12px
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            } */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        font-size: 12px
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    } */
 
         .fs-12 {
             font-size: 12px
@@ -359,37 +359,39 @@
 @endsection
 
 @section('footer')
-    <script>
-        function removeComment(e) {
-            $(e)?.parent()?.parent()?.parent()?.remove();
-        }
+    @auth('user')
+        <script>
+            function removeComment(e) {
+                $(e)?.parent()?.parent()?.parent()?.remove();
+            }
 
-        function formComment(id) {
-            return "<div class='bg-light p-2 reply-form'>" +
-                "<form action='{{ route('user.comment') }}' method='post'>" +
-                `@csrf` +
-                `<input type='hidden' name='parent_id' value='` + id + `'>` +
-                `<input type='hidden' name='post_id' value='{{ $post->id }}'>` +
-                "<div class='d-flex flex-row align-items-start'>" +
-                "<div><img class='rounded-circle' src='{{ auth('user')->user()->avatar }}' width='40'></div>" +
-                "<div class='d-flex flex-column justify-content-start ml-2 w-100'>" +
-                "<p class='d-block font-weight-bold name'>{{ auth()->user()->name }}</p>" +
-                "<textarea class='form-control shadow-none textarea' name='content' placeholder='...Nhập bình luận...'></textarea>" +
-                "</div></div>" +
-                "<div class='mt-2 text-right'>" +
-                "<button type='submit' class='btn btn-primary btn-sm shadow-none' type='button'>Bình luận</button>" +
-                "<div onClick='removeComment(this)' class='btn btn-danger btn-sm ml-1 shadow-none'>Cancel</div>" +
-                "</div></form></div>";
-        }
+            function formComment(id) {
+                return "<div class='bg-light p-2 reply-form'>" +
+                    "<form action='{{ route('user.comment') }}' method='post'>" +
+                    `@csrf` +
+                    `<input type='hidden' name='parent_id' value='` + id + `'>` +
+                    `<input type='hidden' name='post_id' value='{{ $post->id }}'>` +
+                    "<div class='d-flex flex-row align-items-start'>" +
+                    "<div><img class='rounded-circle' src='{{ auth('user')->user()->avatar }}' width='40'></div>" +
+                    "<div class='d-flex flex-column justify-content-start ml-2 w-100'>" +
+                    "<p class='d-block font-weight-bold name'>{{ auth()->user()->name }}</p>" +
+                    "<textarea class='form-control shadow-none textarea' name='content' placeholder='...Nhập bình luận...'></textarea>" +
+                    "</div></div>" +
+                    "<div class='mt-2 text-right'>" +
+                    "<button type='submit' class='btn btn-primary btn-sm shadow-none' type='button'>Bình luận</button>" +
+                    "<div onClick='removeComment(this)' class='btn btn-danger btn-sm ml-1 shadow-none'>Cancel</div>" +
+                    "</div></form></div>";
+            }
 
 
-        $(document).ready(function() {
-            $('.reply-btn').click(function() {
-                let replyId = $(this).data('replyId');
-                let parent = $(this).parent().parent();
-                $('.comment-section').find("div.reply-form").remove();
-                parent.append(formComment(replyId));
+            $(document).ready(function() {
+                $('.reply-btn').click(function() {
+                    let replyId = $(this).data('replyId');
+                    let parent = $(this).parent().parent();
+                    $('.comment-section').find("div.reply-form").remove();
+                    parent.append(formComment(replyId));
+                });
             });
-        });
-    </script>
+        </script>
+    @endauth
 @endsection
